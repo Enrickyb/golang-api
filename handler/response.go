c3925f0ce039deaddaefa91ab0eda52ccbb2b852
Enrickyb <enrickyb@hotmail.com>
2023-09-18 08:29:05 -0400

    feat: documentation with swagger

--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Enrickyb/golang-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,4 +16,14 @@ func sendError(ctx *gin.Context, code int, msg string) {
 func sendSucess(ctx *gin.Context, op string, data interface{}) {
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("%s sucessfully", op), "data": data})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"msg"`
+	ErrorCode string `json:"error_code"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"msg"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
